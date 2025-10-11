6ede360ec12cac7dc13e59f2dab18cd0e43c8ad6
chozix <venechozix@gmail.com>
2025-10-11 20:27:49 -0300

    add a mountpoint in home for convenience

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,12 @@
     options = [ "defaults" "nofail" ]; # nofail = don’t break boot if missing
   };
 
+  fileSystems."/home/drives/hdd1" = {
+    device = "/dev/disk/by-uuid/39f08c2e-6b56-48d2-bf1b-2010b3983340"; # replace with your UUID
+    fsType = "ext4"; # or whatever lsblk shows
+    options = [ "defaults" "nofail" ]; # nofail = don’t break boot if missing
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
