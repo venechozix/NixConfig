39be70be667e788be390b078b83ab92d39bbdf50
chozix <venechozix@gmail.com>
2025-10-11 20:29:42 -0300

    trolled

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
     options = [ "defaults" "nofail" ]; # nofail = don’t break boot if missing
   };
 
-  fileSystems."/home/drives/hdd1" = {
+  fileSystems."/home/chozix/drives/hdd1" = {
     device = "/dev/disk/by-uuid/39f08c2e-6b56-48d2-bf1b-2010b3983340"; # replace with your UUID
     fsType = "ext4"; # or whatever lsblk shows
     options = [ "defaults" "nofail" ]; # nofail = don’t break boot if missing
