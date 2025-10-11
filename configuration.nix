385b768621ac6be8d3a7daadeef5476ebe110dd6
chozix <venechozix@gmail.com>
2025-10-11 20:19:43 -0300

    mount drive, should maybe move that to another file on a later date

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,13 @@
       ./desktops/niri.nix
     ];
 
+
+  fileSystems."/mnt/hdd1" = {
+    device = "/dev/sdb/by-uuid/39f08c2e-6b56-48d2-bf1b-2010b3983340"; # replace with your UUID
+    fsType = "ext4"; # or whatever lsblk shows
+    options = [ "defaults" "nofail" ]; # nofail = don’t break boot if missing
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
