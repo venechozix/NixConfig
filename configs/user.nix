d62e4c6e1683db9ddb42ac49b5ce48f83831b74a
chozix <venechozix@gmail.com>
2025-10-13 12:30:59 -0300

    zsh/OhMyZsh

--- configs/user.nix
+++ configs/user.nix
@@ -1,6 +1,26 @@
 { config, pkgs, ... }: {
 
     users.defaultUserShell = pkgs.zsh;
+    
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+
+        shellAliases = {
+            ll = "ls -l";
+            update = "sudo nixos-rebuild switch --flake ~/NixConfig/";
+        };
+        history.size = 1000;
+        oh-my-zsh = { # "ohMyZsh" without Home Manager
+            enable = true;
+            plugins = [ "git" "thefuck" ];
+            theme = "nicoulaj";
+        };
+    };
+
+
 
     users.users.chozix = {
         isNormalUser = true;
@@ -10,6 +30,6 @@
             kdePackages.kate
             #  thunderbird
         ];
-  };
+    };
 
 }
