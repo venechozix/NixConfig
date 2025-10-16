040b35ebf717cf65cb5ff982894ddc7bf7010adf
chozix <venechozix@gmail.com>
2025-10-16 12:30:31 -0300

    some graphics settings

--- configs/hardware.nix
+++ configs/hardware.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }: {
 
+    services.xserver.videoDrivers = [ "modesetting" ];
+
+    hardware.opengl.enable = true;
+    hardware.opengl.driSupport = true;
+    hardware.opengl.driSupport32Bit = true;
+
     fileSystems."/mnt/hdd1" = {
         device = "/dev/disk/by-uuid/39f08c2e-6b56-48d2-bf1b-2010b3983340"; # replace with your UUID
         fsType = "ext4"; # or whatever lsblk shows
