6cd7481c19a416e01d0dd399a427930927f6d72e
chozix <venechozix@gmail.com>
2025-10-16 14:18:27 -0300

    more testing

--- configs/hardware.nix
+++ configs/hardware.nix
@@ -14,7 +14,6 @@
     ];
     hardware.graphics = {
         enable = true;
-        driSupport32Bit = true;
         extraPackages = with pkgs; [
             mesa.drivers
             intel-vaapi-driver
