17d3136920938f8e44747e715f138614acfc15dc
chozix <venechozix@gmail.com>
2025-10-16 12:56:13 -0300

    test

--- configs/hardware.nix
+++ configs/hardware.nix
@@ -3,10 +3,12 @@
     services.xserver.enable = true;
     services.xserver.videoDrivers = [ "modesetting" ];
 
-    hardware.opengl = {
+    nixpkgs.config.packageOverrides = pkgs: {
+        intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+    };
+    hardware.graphics = {
         enable = true;
         extraPackages = with pkgs; [
-            intel-media-driver
             intel-vaapi-driver
             libvdpau-va-gl
         ];
