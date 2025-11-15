8db5dbca86c79d3c9423b6ac48d5e23c680b39e5
chozix <venechozix@gmail.com>
2025-11-15 15:11:17 -0300

    test

--- flake.nix
+++ flake.nix
@@ -1,29 +1,45 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        home-manager = {
-              url = "github:nix-community/home-manager";
-              inputs.nixpkgs.follows = "nixpkgs";
-          };
-        zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    };
-    outputs = { nixpkgs, home-manager, zen-browser, ... } @ inputs: {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-            modules = [ 
-                ./configuration.nix
-                home-manager.nixosModules.home-manager
-                {
-                    home-manager = {
-                      useGlobalPkgs = true;
-                      useUserPackages = true;
-                      users.chozix = import ./home.nix;
-                      backupFileExtension = "backup";
-                      extraSpecialArgs = { inherit inputs; };
-                    };
-                } 
-            ];
-        };
+    # Use the specified commit where gfn-electron existed
+    oldpkgs.url = "github:nixos/nixpkgs/3acb677ea67d4c6218f33de0db0955f116b7588c";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+
+  outputs = { nixpkgs, home-manager, zen-browser, oldpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+
+        # overlay for gfn-electron
+        {
+          nixpkgs.overlays = [
+            (final: prev: {
+              gfn-electron = (import oldpkgs { system = "x86_64-linux"; }).gfn-electron;
+            })
+          ];
+        }
+
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.chozix = import ./home.nix;
+            backupFileExtension = "backup";
+            extraSpecialArgs = { inherit inputs; };
+          };
+        }
+      ];
+    };
+  };
 }
