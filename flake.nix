f4f3250dafa7f453d0868f75a511cb4b7d6a5957
chozix <venechozix@gmail.com>
2026-01-05 11:07:43 -0300

    change nixpkgs from unstable to 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
 
     home-manager = {
-        url = "github:nix-community/home-manager";
+        url = "github:nix-community/home-manager/release-25.11";
         inputs.nixpkgs.follows = "nixpkgs";
     };
 
