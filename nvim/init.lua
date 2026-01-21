8ec1f73738e0146f830552f9aafb74b034f8b5f7
chozix <venechozix@gmail.com>
2026-01-21 19:00:57 -0300

    oopsie

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("plugins.telescope")
 require("plugins.treesitter")
 require("plugins.lsp")
 require("plugins.cmp")
+require("plugins.format")
 -- General Configurations
 
 vim.cmd("colorscheme catppuccin_macchiato")
