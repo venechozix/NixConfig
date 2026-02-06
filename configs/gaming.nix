728419b8a79ec0fb3ef3ad0b9d49603bb5d1f843
chozix <venechozix@gmail.com>
2026-02-06 20:23:53 -0300

    gamescope

--- configs/gaming.nix
+++ configs/gaming.nix
@@ -1,37 +1,43 @@
-{ config, pkgs, inputs,  ... }: {
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
 
-    
-    #Steam
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true;
-        dedicatedServer.openFirewall = true;
-        extraCompatPackages =  with pkgs; [
-            proton-ge-bin
-        ];
-        protontricks.enable = true;
-    };
-
-    programs.appimage.enable = true;
-    programs.appimage.binfmt = true;
-
-    hardware.opentabletdriver = {
-        enable = true;
-        daemon.enable = true;
-    };
-
-    environment.systemPackages = with pkgs; [
-        #gaming
-        ryubing
-        protonup-qt
-        opentabletdriver
-        osu-lazer-bin
-        lutris
-        heroic-unwrapped
-        glib
-        glib-networking
-        prismlauncher-unwrapped
-        dconf
+  #Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
     ];
+    protontricks.enable = true;
+  };
+
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+
+  hardware.opentabletdriver = {
+    enable = true;
+    daemon.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    #gaming
+    gamescope
+    ryubing
+    protonup-qt
+    opentabletdriver
+    osu-lazer-bin
+    lutris
+    heroic-unwrapped
+    glib
+    glib-networking
+    prismlauncher-unwrapped
+    dconf
+  ];
 
 }
