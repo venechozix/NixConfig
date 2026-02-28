296e6ad016c94424e92c815688e6dcf730d6cec7
chozix <venechozix@gmail.com>
2026-02-28 19:53:51 -0300

    eog

--- configs/niri.nix
+++ configs/niri.nix
@@ -1,36 +1,38 @@
-{config, pkgs, ...}:{
-    programs.niri.enable=true;
-    
-    services.gnome.gnome-keyring.enable = true;
-    security.polkit.enable = true;
+{ config, pkgs, ... }:
+{
+  programs.niri.enable = true;
 
-    xdg.portal = {
-        enable = true;
-        extraPortals = with pkgs ; [
-            xdg-desktop-portal-gtk  
-            xdg-desktop-portal-gnome
-        ];
-        xdgOpenUsePortal = true;
-    };
+  services.gnome.gnome-keyring.enable = true;
+  security.polkit.enable = true;
 
-   environment.sessionVariables = {
-       # 1. Forces native Wayland
-       SDL_VIDEODRIVER = "wayland"; 
-
-       # 2. Crucial fix: Disables the GTK-style title bar (libdecor)
-       SDL_VIDEO_WAYLAND_PREFER_LIBDECOR = "0"; 
-   };
-
-    environment.systemPackages = with pkgs; [
-        polkit_gnome
-        swww
-        wlr-randr
-        nautilus
-        waybar
-        fuzzel
-        mako
-        swaybg
-        swayidle
-        xwayland-satellite
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
     ];
+    xdgOpenUsePortal = true;
+  };
+
+  environment.sessionVariables = {
+    # 1. Forces native Wayland
+    SDL_VIDEODRIVER = "wayland";
+
+    # 2. Crucial fix: Disables the GTK-style title bar (libdecor)
+    SDL_VIDEO_WAYLAND_PREFER_LIBDECOR = "0";
+  };
+
+  environment.systemPackages = with pkgs; [
+    polkit_gnome
+    swww
+    wlr-randr
+    nautilus
+    waybar
+    fuzzel
+    mako
+    swaybg
+    swayidle
+    xwayland-satellite
+    eog
+  ];
 }
