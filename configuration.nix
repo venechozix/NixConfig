058de72509d32e96e22a2277c7f427886c2ae06f
chozix <venechozix@gmail.com>
2026-03-10 00:46:21 -0300

    test gdm again after flipping monitors

--- configuration.nix
+++ configuration.nix
@@ -38,15 +38,18 @@
   # Set your time zone.
   time.timeZone = "America/Montevideo";
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
-        user = "greeter";
-      };
-    };
-  };
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
+
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
