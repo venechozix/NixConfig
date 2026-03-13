9e7a66c7bc806e166f777b2fcf16087c8342dd93
chozix <venechozix@gmail.com>
2026-03-12 21:47:21 -0300

    change kernel to 6.19

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
     ./configs/niri.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_6_16;
+  boot.kernelPackages = pkgs.pkgs.linuxPackages_6_19;
 
   boot.initrd.kernelModules = [ "amdgpu" ];
 
