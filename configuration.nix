bc5624a34511bd097b61f93a4abf56aaf1b5ca3a
chozix <venechozix@gmail.com>
2026-03-17 12:44:33 -0300

    oops

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,14 @@
       };
     };
 
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+      };
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
